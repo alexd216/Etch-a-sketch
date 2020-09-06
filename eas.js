14ac9bf30e31f9fd8638b5c65c99c7294bb0ca5f
alex <alex_davies82@hotmail.com>
2020-09-06 13:07:07 +0100

    added random colour generator and start game button to home screen

--- eas.js
+++ eas.js
@@ -1,18 +1,20 @@
-
-
-
 // create container and variables needed to set the size of the boxes
 const container = document.querySelector('#container');
 const w = window.innerWidth;
 const h = window.innerHeight;
-const num = prompt('select the size of the grid');
+let num = 0;
+let colour = 0;
+// create button to refresh page and ask the user for the size of the grid
+newGame.addEventListener('click', function(){
+    container.innerHTML = ''
+    num = prompt('How many sizes would you like in your grid?')
+    grid(num);
+})
 
-console.log(w);
 // function that creates the boxes for one line across the screen
 function createBlock(num) {
     wid = (w/num - 5);
     hei = (h/num - 5);
-    console.log(wid);
     while (num > 0) {
     const block = document.createElement('div');
     block.classList.add('block');
@@ -25,13 +27,16 @@ function createBlock(num) {
     block.style.border = 'solid black 0';
     block.style.padding = '0 0 0 0';
     block.addEventListener('mouseover', function() {
-        block.style.backgroundColor = 'yellow';
+        block.style.backgroundColor = colour;
+        ;
     })
     container.appendChild(block);
     num --;
+    colour = '#'+(0x1000000+(Math.random())*0xffffff).toString(16).substr(1,6)
     }
 }
+
 //this function runs the above script to make the grid
-function grid() {
+function grid(num) {
 for (let i = 0; i < num; i++) createBlock(num);
 }
