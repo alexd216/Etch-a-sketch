a71b0976b316d208b90834bd77e326b9f054b9c2
alex <alex_davies82@hotmail.com>
2020-09-06 12:01:30 +0100

    created funcitons to size the boxes based off user input

--- eas.js
+++ eas.js
@@ -1,21 +1,37 @@
-const container = document.querySelector('#container');
 
+
+
+// create container and variables needed to set the size of the boxes
+const container = document.querySelector('#container');
+const w = window.innerWidth;
+const h = window.innerHeight;
+const num = prompt('select the size of the grid');
+
+console.log(w);
+// function that creates the boxes for one line across the screen
 function createBlock(num) {
+    wid = (w/num - 5);
+    hei = (h/num - 5);
+    console.log(wid);
     while (num > 0) {
     const block = document.createElement('div');
     block.classList.add('block');
+    block.id = num;
     block.textContent = '';
     block.style.display = 'inline-table';
-    block.style.height = '70px';
-    block.style.width = '100px';
-    block.style.margin = '1px 1px 1px 1px';
-    block.style.border = 'solid black';
-    block.addEventListener('mouseover', funciton() {
-        style.backgroundColor = 'yellow';
+    block.style.height = hei + 'px';
+    block.style.width = wid + 'px';
+    block.style.margin = '0 0 0 0';
+    block.style.border = 'solid black 0';
+    block.style.padding = '0 0 0 0';
+    block.addEventListener('mouseover', function() {
+        block.style.backgroundColor = 'yellow';
     })
     container.appendChild(block);
     num --;
     }
 }
-
-for (let i = 1; i < 16; i++) createBlock(16);
+//this function runs the above script to make the grid
+function grid() {
+for (let i = 0; i < num; i++) createBlock(num);
+}
